316633fe7434d0115268584df2661b404cbfb568
vanessavilchis <vanessavilchis.info@gmail.com>
2025-10-20 15:11:47 -0400

    Second Commit - Room and Reservation class created.

--- src/main/java/com/pluralsight/Reservation.java
+++ src/main/java/com/pluralsight/Reservation.java
@@ -1,5 +1,59 @@
 package com.pluralsight;
 
 public class Reservation {
-    private String
+    private String roomType;
+    private double price;
+    private int numberOfNights;
+    private boolean isWeekend;
+
+    public String getRoomType() {
+        return roomType;
+    }
+    public void setRoomType(String roomType) {
+        this.roomType = roomType;
+    }
+
+    public double getReservationPrice() {
+        return price;
+    }
+
+    public int getNumberOfNights() {
+        return numberOfNights;
+    }
+
+    public void setNumberOfNights(int numberOfNights) {
+        this.numberOfNights = numberOfNights;
+    }
+
+    public boolean isWeekend() {
+        return isWeekend;
+    }
+
+    public void setWeekend(boolean weekend) {
+        this.isWeekend = weekend;
+    }
+    public Reservation(String roomType, int numberOfNights, boolean weekend) {
+        this.roomType = roomType;
+        switch (roomType){
+            case "king":
+                this.price = 139;
+                break;
+            case "double":
+                this.price = 124;
+                break;
+        }
+       this.numberOfNights = numberOfNights;
+        this.isWeekend = isWeekend;
+    }
+
+    public double getReservationTotal() {
+        double reservationTotal = price*numberOfNights;
+        if (isWeekend()){
+            reservationTotal *= 1.1;
+        }
+        return reservationTotal;
+    }
+
 }
+
+
